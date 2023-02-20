636e5f8ceb4a9bfee66cf66653e33b5c47ed32ef
peterino <peterlimail47@gmail.com>
2023-02-20 02:40:07 -0800

    comments

--- engine/modules/graphics/vk_utils.zig
+++ engine/modules/graphics/vk_utils.zig
@@ -29,23 +29,6 @@ pub fn load_image_from_file(ctx: *NeonVkContext, filePath: []const u8) !NeonVkIm
     // but calling VkQueueSubmit is not going to be threadsafe unless we create a
     // seperate command pool for each thread.
 
-    // var h: c_int = -1;
-    // var w: c_int = -1;
-    // var comp: c_int = -1;
-    // var pixels: ?*u8 = core.stbi_load(
-    //     filePath.ptr,
-    //     &w,
-    //     &h,
-    //     &comp,
-    //     core.STBI_rgb_alpha,
-    // );
-    // if (pixels == null) {
-    //     core.engine_log("Unable to load file from path {s}", .{filePath});
-    //     return error.UnableToLoadFile;
-    // }
-
-    // ctx.stbiLoaderMutex.unlock();
-
     var pngFileContents = try core.loadFileAlloc(filePath, 8, ctx.allocator);
     var decoder = try spng.SpngContext.newDecoder();
     try decoder.setBuffer(pngFileContents);
@@ -95,100 +78,96 @@ pub fn load_image_from_file(ctx: *NeonVkContext, filePath: []const u8) !NeonVkIm
         .pixelHeight = imageExtent.height,
     };
 
-    // try ctx.start_upload_context(&ctx.uploadContext);
-    // var newImage = NeonVkImage{
-    //     .image = result.image,
-    //     .allocation = result.allocation,
-    //     .pixelWidth = imageExtent.width,
-    //     .pixelHeight = imageExtent.height,
-    // };
-    // {
-    //     var cmd = ctx.uploadContext.commandBuffer;
-    //     var range = vk.ImageSubresourceRange{
-    //         .aspect_mask = .{ .color_bit = true },
-    //         .base_mip_level = 0,
-    //         .level_count = 1,
-    //         .base_array_layer = 0,
-    //         .layer_count = 1,
-    //     };
+    // this part below here is unsafe. return now to make this function
+    // safe... and useless
+    try ctx.start_upload_context(&ctx.uploadContext);
+    {
+        var cmd = ctx.uploadContext.commandBuffer;
+        var range = vk.ImageSubresourceRange{
+            .aspect_mask = .{ .color_bit = true },
+            .base_mip_level = 0,
+            .level_count = 1,
+            .base_array_layer = 0,
+            .layer_count = 1,
+        };
 
-    //     var imageBarrier_toTransfer = vk.ImageMemoryBarrier{
-    //         .old_layout = .undefined,
-    //         .new_layout = .transfer_dst_optimal,
-    //         .image = newImage.image,
-    //         .subresource_range = range,
-    //         .src_access_mask = .{},
-    //         .dst_access_mask = .{
-    //             .transfer_write_bit = true,
-    //         },
-    //         .src_queue_family_index = 0,
-    //         .dst_queue_family_index = 0,
-    //     };
-    //     ctx.vkd.cmdPipelineBarrier(
-    //         cmd,
-    //         .{ .top_of_pipe_bit = true },
-    //         .{ .transfer_bit = true },
-    //         .{},
-    //         0,
-    //         undefined,
-    //         0,
-    //         undefined,
-    //         1,
-    //         p2a(&imageBarrier_toTransfer),
-    //     );
+        var imageBarrier_toTransfer = vk.ImageMemoryBarrier{
+            .old_layout = .undefined,
+            .new_layout = .transfer_dst_optimal,
+            .image = newImage.image,
+            .subresource_range = range,
+            .src_access_mask = .{},
+            .dst_access_mask = .{
+                .transfer_write_bit = true,
+            },
+            .src_queue_family_index = 0,
+            .dst_queue_family_index = 0,
+        };
+        ctx.vkd.cmdPipelineBarrier(
+            cmd,
+            .{ .top_of_pipe_bit = true },
+            .{ .transfer_bit = true },
+            .{},
+            0,
+            undefined,
+            0,
+            undefined,
+            1,
+            p2a(&imageBarrier_toTransfer),
+        );
 
-    //     var copyRegion = vk.BufferImageCopy{
-    //         .buffer_offset = 0,
-    //         .buffer_row_length = 0,
-    //         .buffer_image_height = 0,
-    //         .image_offset = std.mem.zeroes(vk.Offset3D),
-    //         .image_subresource = .{
-    //             .aspect_mask = .{ .color_bit = true },
-    //             .mip_level = 0,
-    //             .base_array_layer = 0,
-    //             .layer_count = 1,
-    //         },
-    //         .image_extent = imageExtent,
-    //     };
+        var copyRegion = vk.BufferImageCopy{
+            .buffer_offset = 0,
+            .buffer_row_length = 0,
+            .buffer_image_height = 0,
+            .image_offset = std.mem.zeroes(vk.Offset3D),
+            .image_subresource = .{
+                .aspect_mask = .{ .color_bit = true },
+                .mip_level = 0,
+                .base_array_layer = 0,
+                .layer_count = 1,
+            },
+            .image_extent = imageExtent,
+        };
 
-    //     ctx.vkd.cmdCopyBufferToImage(
-    //         cmd,
-    //         stagingBuffer.buffer,
-    //         newImage.image,
-    //         .transfer_dst_optimal,
-    //         1,
-    //         p2a(&copyRegion),
-    //     );
+        ctx.vkd.cmdCopyBufferToImage(
+            cmd,
+            stagingBuffer.buffer,
+            newImage.image,
+            .transfer_dst_optimal,
+            1,
+            p2a(&copyRegion),
+        );
 
-    //     var imageBarrier_toReadable = vk.ImageMemoryBarrier{
-    //         .old_layout = .transfer_dst_optimal,
-    //         .new_layout = .shader_read_only_optimal,
-    //         .image = newImage.image,
-    //         .subresource_range = range,
-    //         .src_access_mask = .{
-    //             .transfer_write_bit = true,
-    //         },
-    //         .dst_access_mask = .{
-    //             .shader_read_bit = false,
-    //         },
-    //         .src_queue_family_index = 0,
-    //         .dst_queue_family_index = 0,
-    //     };
+        var imageBarrier_toReadable = vk.ImageMemoryBarrier{
+            .old_layout = .transfer_dst_optimal,
+            .new_layout = .shader_read_only_optimal,
+            .image = newImage.image,
+            .subresource_range = range,
+            .src_access_mask = .{
+                .transfer_write_bit = true,
+            },
+            .dst_access_mask = .{
+                .shader_read_bit = false,
+            },
+            .src_queue_family_index = 0,
+            .dst_queue_family_index = 0,
+        };
 
-    //     ctx.vkd.cmdPipelineBarrier(
-    //         cmd,
-    //         .{ .transfer_bit = true },
-    //         .{ .fragment_shader_bit = true },
-    //         .{},
-    //         0,
-    //         undefined,
-    //         0,
-    //         undefined,
-    //         1,
-    //         p2a(&imageBarrier_toReadable),
-    //     );
-    // }
-    // try ctx.finish_upload_context(&ctx.uploadContext);
+        ctx.vkd.cmdPipelineBarrier(
+            cmd,
+            .{ .transfer_bit = true },
+            .{ .fragment_shader_bit = true },
+            .{},
+            0,
+            undefined,
+            0,
+            undefined,
+            1,
+            p2a(&imageBarrier_toReadable),
+        );
+    }
+    try ctx.finish_upload_context(&ctx.uploadContext);
 
     return newImage;
 }
