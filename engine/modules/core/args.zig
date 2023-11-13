020eee657b992961ff1bbb4f984c0b8cad41fd55
Peter Li <peterlimail47@gmail.com>
2023-11-13 13:12:02 -0800

    reduced it down to a single function

--- engine/modules/core/args.zig
+++ engine/modules/core/args.zig
@@ -2,104 +2,96 @@
 
 const std = @import("std");
 
-pub fn ArgParse(comptime T: type) type {
-    return struct {
-        args: T,
-        allocator: std.mem.Allocator,
+pub fn ParseArgs(comptime T: type, argList: []const []const u8) !T {
+    var args: T = .{};
+    var shortBuf: [32]u8 = undefined;
+    _ = shortBuf;
+    var longBuf: [256]u8 = undefined;
 
-        pub fn Parse(allocator: std.mem.Allocator, argList: []const []const u8) !*@This() {
-            var self: *@This() = try allocator.create(@This());
-            self.args = .{};
-            self.allocator = allocator;
-            var shortBuf: [32]u8 = undefined;
-            _ = shortBuf;
-            var longBuf: [256]u8 = undefined;
+    var i: u32 = 0;
+    while (i < argList.len) {
+        var a = argList[i];
 
-            var i: u32 = 0;
-            while (i < argList.len) {
-                var a = argList[i];
+        inline for (@typeInfo(T).Struct.fields) |field| {
+            var longRef = try std.fmt.bufPrint(&longBuf, "--{s}", .{field.name});
 
-                inline for (@typeInfo(T).Struct.fields) |field| {
-                    var longRef = try std.fmt.bufPrint(&longBuf, "--{s}", .{field.name});
-
-                    if (std.mem.startsWith(u8, a, longRef)) {
-                        // check if it has more arguments.
-                        if (a.len > longRef.len) {
-                            if (a[longRef.len] == '=' and a.len > longRef.len + 1) {
-                                var right = a[longRef.len + 1 ..];
-                                switch (@typeInfo(field.type)) {
-                                    .Bool => {
-                                        if (std.mem.eql(u8, right, "true") or
-                                            std.mem.eql(u8, right, "t") or
-                                            std.mem.eql(u8, right, "True") or
-                                            std.mem.eql(u8, right, "T") or
-                                            std.mem.eql(u8, right, "1"))
-                                        {
-                                            @field(self.args, field.name) = true;
-                                        } else {
-                                            @field(self.args, field.name) = false;
-                                        }
-                                    },
-                                    .Int => {
-                                        @field(self.args, field.name) = try std.fmt.parseInt(field.type, right, 10);
-                                    },
-                                    .Float => {
-                                        @field(self.args, field.name) = try std.fmt.parseFloat(field.type, right);
-                                    },
-                                    .Pointer => |pointer| {
-                                        switch (pointer.Size) {
-                                            .Slice => {
-                                                // i'm just going to assume that it's a u8 slice, i don't support other types here.
-                                            },
-                                            else => {},
-                                        }
+            if (std.mem.startsWith(u8, a, longRef)) {
+                // check if it has more arguments.
+                if (a.len > longRef.len) {
+                    if (a[longRef.len] == '=' and a.len > longRef.len + 1) {
+                        var right = a[longRef.len + 1 ..];
+                        switch (@typeInfo(field.type)) {
+                            .Bool => {
+                                if (std.mem.eql(u8, right, "true") or
+                                    std.mem.eql(u8, right, "t") or
+                                    std.mem.eql(u8, right, "True") or
+                                    std.mem.eql(u8, right, "T") or
+                                    std.mem.eql(u8, right, "1"))
+                                {
+                                    @field(args, field.name) = true;
+                                } else {
+                                    @field(args, field.name) = false;
+                                }
+                            },
+                            .Int => {
+                                @field(args, field.name) = try std.fmt.parseInt(field.type, right, 10);
+                            },
+                            .Float => {
+                                @field(args, field.name) = try std.fmt.parseFloat(field.type, right);
+                            },
+                            .Pointer => |pointer| {
+                                switch (pointer.size) {
+                                    .Slice => {
+                                        // i'm just going to assume that it's a u8 slice, i don't support other types here.
+                                        @field(args, field.name) = right;
                                     },
                                     else => {
-                                        @compileError("unable to generate argparse, unsupported struct type in field: " ++ field.name);
+                                        @compileError("only u8 slices are supported");
                                     },
                                 }
-                            } else {
-                                std.debug.print("Error parsing arguments: Expected value after argument assignment '{s}'\n", .{a});
-                                return error.MalformedArgument;
-                            }
-                        } else {
-                            switch (@typeInfo(field.type)) {
-                                .Bool => {
-                                    @field(self.args, field.name) = true;
-                                },
-                                .Int, .Float => {
+                            },
+                            else => {
+                                @compileError("unable to generate argparse, unsupported struct type in field: " ++ field.name);
+                            },
+                        }
+                    } else {
+                        std.debug.print("Error parsing arguments: Expected value after argument assignment '{s}'\n", .{a});
+                        return error.MalformedArgument;
+                    }
+                } else {
+                    switch (@typeInfo(field.type)) {
+                        .Bool => {
+                            @field(args, field.name) = true;
+                        },
+                        .Int, .Float => {
+                            std.debug.print("Error parsing arguments: Expected value '{s}'\n", .{a});
+                            return error.MalformedArgument;
+                        },
+                        .Pointer => |pointer| {
+                            switch (pointer.size) {
+                                .Slice => {
+                                    // i'm just going to assume that it's a u8 slice, i don't support other types here.
                                     std.debug.print("Error parsing arguments: Expected value '{s}'\n", .{a});
                                     return error.MalformedArgument;
                                 },
-                                .Pointer => |pointer| {
-                                    switch (pointer.Size) {
-                                        .Slice => {
-                                            // i'm just going to assume that it's a u8 slice, i don't support other types here.
-                                            std.debug.print("Error parsing arguments: Expected value '{s}'\n", .{a});
-                                            return error.MalformedArgument;
-                                        },
-                                        else => {},
-                                    }
-                                },
                                 else => {
-                                    @compileError("unable to generate argparse, unsupported struct type in field: " ++ field.name);
+                                    @compileError("only u8 slices are supported");
                                 },
                             }
-                        }
-                        break;
+                        },
+                        else => {
+                            @compileError("unable to generate argparse, unsupported struct type in field: " ++ field.name);
+                        },
                     }
                 }
-
-                i += 1;
+                break;
             }
-
-            return self;
         }
 
-        pub fn deinit(self: *@This()) void {
-            self.allocator.destroy(self);
-        }
-    };
+        i += 1;
+    }
+
+    return args;
 }
 
 test "test args" {
@@ -107,12 +99,13 @@ test "test args" {
         foo: f32 = 0,
         bar: i32 = 0,
         baz: bool = false,
+        test_file: []const u8 = "",
     };
 
-    var x = try ArgParse(A).Parse(std.testing.allocator, &.{ "--foo=32", "--bar=22", "--baz" });
-    std.debug.assert(x.args.foo == 32);
-    std.debug.assert(x.args.bar == 22);
-    std.debug.assert(x.args.baz == true);
+    var args = try ParseArgs(A, &.{ "--foo=32", "--bar=22", "--baz", "--test_file=lmao2nova" });
 
-    defer x.deinit();
+    std.debug.assert(args.foo == 32);
+    std.debug.assert(args.bar == 22);
+    std.debug.assert(args.baz == true);
+    std.debug.assert(std.mem.eql(u8, args.test_file, "lmao2nova"));
 }
