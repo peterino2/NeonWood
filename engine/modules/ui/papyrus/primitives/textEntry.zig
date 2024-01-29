6cc29cb5aada9bcebdf4f738d38b6c8fe6e587a8
peterino <peterlimail47@gmail.com>
2024-01-28 20:38:13 -0800

    saving before merge

--- engine/modules/ui/papyrus/primitives/textEntry.zig
+++ engine/modules/ui/papyrus/primitives/textEntry.zig
@@ -4,6 +4,15 @@ disabled: bool = false,
 entryState: TextEntryState = .Normal,
 entryStubPosition: u32 = 0,
 
+pub const TextEntryStyle = struct {
+    normal: papyrus.PapyrusNodeStyle = .{
+        .borderColor = BurnStyle.Diminished,
+    },
+    typing: papyrus.PapyrusNodeStyle = .{
+        .borderColor = BurnStyle.LightGrey,
+    },
+};
+
 const std = @import("std");
 const papyrus = @import("../papyrus.zig");
 const LocText = papyrus.LocText;
@@ -22,6 +31,18 @@ pub const TextEntryState = enum {
     Pressed,
 };
 
+pub const TextEntrySystem = struct {
+    node: papyrus.NodeHandle,
+};
+
 pub fn addToDrawList(dlb: DrawListBuilder) !void {
-    _ = dlb;
+    var drawlist = dlb.drawList;
+    const button = dlb.n.nodeType.Button;
+
+    // 0. select colors
+    // 1. draw the rect
+    // 2.
+
+    _ = drawlist;
+    _ = button;
 }
